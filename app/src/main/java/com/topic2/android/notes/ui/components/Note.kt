fe9f34ad3f23972ebb2b4a63506df7e4b1e42edd
Anastasia <3778242nastya@gmail.com>
2024-03-22 14:33:01 +0300

    add NoteColor in notes

--- app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
@@ -1,12 +1,11 @@
 package com.topic2.android.notes.ui.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -17,10 +16,11 @@ import com.topic2.android.notes.theme.rwGreen
 
 @Composable fun Note(){
     Row(modifier = Modifier.fillMaxWidth()){
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .background(rwGreen)
+        NoteColor(
+            color = rwGreen ,
+            size = 40.dp ,
+            padding = 4.dp,
+            border = 1.dp
         )
         Column(modifier = Modifier.weight(1f)) {
             Text(text = "Загаловок", maxLines = 1)
