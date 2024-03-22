558eb838b75704be1e18e74ffc36eb888500c0df
Anastasia <3778242nastya@gmail.com>
2024-03-22 14:48:45 +0300

    add notes with background in file Note.kt

--- app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
@@ -1,21 +1,32 @@
 package com.topic2.android.notes.ui.components
 
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.topic2.android.notes.theme.rwGreen
 
 @Composable fun Note(){
-    Row(modifier = Modifier.fillMaxWidth()){
+val backgroundShape: Shape = RoundedCornerShape(4.dp)
+    Row(modifier = Modifier
+        .padding(8.dp)
+        .shadow(1.dp, backgroundShape)
+        .heightIn(min  = 64.dp)
+        .fillMaxWidth()
+        .background(Color.White, backgroundShape)){
         NoteColor(
             color = rwGreen ,
             size = 40.dp ,
